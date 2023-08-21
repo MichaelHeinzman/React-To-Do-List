a3cf0eae88f0cd3a375b39277350d3486bd98f0f
Seth Heinzman <sethheinzman@Seths-MacBook-Pro.local>
2023-08-21 13:36:42 -0400

    Changed form styling

--- src/components/ListContainer/ListItemForm.jsx
+++ src/components/ListContainer/ListItemForm.jsx
@@ -20,7 +20,7 @@ const ListItemForm = ({ disableForm }) => {
 
   return (
     <div
-      className="flex w-full flex-col gap-y-4 self-center rounded-md bg-[#4a4343] p-3 shadow-md shadow-[#3f3f3f4e]"
+      className="flex w-full flex-col gap-y-4 self-center rounded-md bg-[#ffffff] p-3 shadow-md shadow-[#3f3f3f4e]"
       data-testid="list-item-form"
     >
       <h3 className="text-center text-lg font-bold text-white">
@@ -31,7 +31,7 @@ const ListItemForm = ({ disableForm }) => {
       <div className="mb-6 flex-col gap-2">
         <div className="md:w-1/3">
           <label
-            className="mb-1 block pr-4 font-bold text-gray-200 md:mb-0"
+            className="mb-1 block pr-4 font-bold text-black md:mb-0"
             htmlFor="inline-full-name"
           >
             Title
@@ -39,7 +39,7 @@ const ListItemForm = ({ disableForm }) => {
         </div>
         <div className="md:w-full">
           <input
-            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-[#5a7779] focus:bg-white focus:outline-none"
+            className="w-full appearance-none rounded border-2 border-[#797979] bg-gray-100 px-4 py-2 leading-tight text-gray-700 focus:border-[#000000] focus:bg-white focus:text-black focus:outline-none"
             id="inline-full-name"
             type="text"
             placeholder="Clean Room"
@@ -53,33 +53,33 @@ const ListItemForm = ({ disableForm }) => {
       <div className="mb-6 flex-col gap-2">
         <div className="md:w-1/3">
           <label
-            className="mb-1 block pr-4 font-bold text-gray-200 md:mb-0 md:text-left"
-            htmlFor="inline-password"
+            className="mb-1 block pr-4 font-bold text-black md:mb-0 md:text-left"
+            htmlFor="inline-description"
           >
             Description
           </label>
         </div>
         <div className="md:w-full">
           <input
-            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-[#5a7779] focus:bg-white focus:outline-none"
-            id="inline-password"
+            className="w-full appearance-none rounded border-2 border-[#797979] bg-gray-100 px-4 py-2 leading-tight text-gray-700 focus:border-[#000000] focus:bg-white focus:text-black focus:outline-none"
+            id="inline-description"
             type="text"
-            placeholder="******************"
-            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
             value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </div>
       </div>
 
       <div className="flex w-full gap-3 self-center">
         <button
-          className="w-1/2 self-center rounded-md bg-gray-200 p-2 text-sm font-semibold text-green-400 hover:bg-green-400 hover:text-gray-200"
+          className="w-1/2 self-center rounded-md bg-black p-2 text-sm font-semibold text-white hover:bg-green-400"
           onClick={onFormSubmit}
         >
           Submit
         </button>
         <button
-          className="w-1/2 self-center rounded-md bg-gray-200 p-2 text-sm font-semibold text-red-400 hover:bg-red-400 hover:text-gray-200"
+          className="w-1/2 self-center rounded-md bg-black p-2 text-sm font-semibold text-white hover:bg-red-400 hover:text-gray-200"
           onClick={disableForm}
         >
           Cancel
